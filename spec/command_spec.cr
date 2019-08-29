22a8011bd3ce41df85a15890ae93e52a94584da6
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2019-08-29 18:43:46 -0300

    Update specs

--- spec/command_spec.cr
+++ spec/command_spec.cr
@@ -2,8 +2,7 @@ require "./spec_helper"
 
 describe Tesseract::Ocr::Command do
   it "make options" do
-    instance = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout", {} of Symbol => String)
-
+    instance = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout")
     instance.make_option("psm", nil).should eq(nil)
     instance.make_option("psm", 1).should eq("--psm 1")
     instance.make_option("psm", "1").should eq("--psm 1")
@@ -11,7 +10,7 @@ describe Tesseract::Ocr::Command do
   end
 
   it "make sort options" do
-    instance = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout", {} of Symbol => String)
+    instance = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout")
 
     instance.make_short_option("l", nil).should eq(nil)
     instance.make_short_option("l", "por").should eq("-l por")
@@ -20,15 +19,16 @@ describe Tesseract::Ocr::Command do
   end
 
   it "join all options to a command" do
-    command = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout", {
+    command = Tesseract::Ocr::Command.new("./spec/resources/blank.tif", "stdout")
+    command.add_options({
       :config_path => "/tmp/config_path.txt",
       :l           => "eng",
       :config      => [
         "a=1",
         "b=2",
       ],
-    }).command
+    })
 
-    command.should eq("tesseract ./spec/resources/blank.tif stdout -l eng -c a=1 -c b=2 /tmp/config_path.txt")
+    command.command.should eq("tesseract ./spec/resources/blank.tif stdout -l eng -c a=1 -c b=2 /tmp/config_path.txt")
   end
 end
